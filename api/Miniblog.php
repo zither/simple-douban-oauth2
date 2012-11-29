9c87632cc145cac6c393c4f7f827731f71360193
JonChou <ilorn.mc@gmail.com>
2012-11-29 11:34:44 +0800

    update Miniblog api

--- api/Miniblog.php
+++ api/Miniblog.php
@@ -30,4 +30,77 @@ class Miniblog extends Base {
         return $this;   
     }
     
+    /**
+     * @brief 获取当前登录用户及其所关注用户的最新广播（友邻广播）
+     *
+     * @param string $accessToken
+     * @param string $sinceId
+     * @param string $untilId
+     * @param string $count
+     * @param string $start
+     *
+     * @return object
+     */
+    public function homeTimeline($accessToken, $sinceId = null, $untilId = null, $count = null, $start = null )
+    {
+        $this->uri = "/shuo/v2/statuses/home_timeline?since_id=$sinceId&until_id=$untilId
+            &count=$count&start=$start";
+
+        $this->header = array(
+                'Authorization: Bearer '.$accessToken
+                );
+
+        return $this;
+
+    }
+    
+    /**
+     * @brief 获取用户发布的广播列表
+     *
+     * @param string $user
+     * @param string $sinceId
+     * @param string $untilId
+     *
+     * @return object
+     */
+    public function userTimeline($user, $sinceId = null, $untilId = null)
+    {
+        $this->uri = "/shuo/v2/statuses/user_timeline/$user?since_id=$sinceId&until_id=$untilId";
+
+        return $this;
+    }
+    
+    /**
+     * @brief 单独读取一条广播
+     *
+     * @param string $id
+     *
+     * @return object
+     */
+    public function get($id)
+    {
+        $this->uri = "/shuo/v2/statuses/$id";
+        
+        return $this;
+    }
+    
+    /**
+     * @brief 删除一条广播
+     *
+     * @param string $accessToken
+     * @param string $id
+     *
+     * @return object
+     */
+    public function delete($accessToken, $id)
+    {
+        $this->uri = "/shuo/v2/statuses/$id";
+
+        $this->header = array("Authorization: Bearer $accessToken");
+
+        $this->type = 'DELETE';
+
+        return $this;
+    }
+    
 }
